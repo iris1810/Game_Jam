1dc6886fa0434c39ecfb7f2824cfd103711c065a
Voldermen <153773095+Voldermen@users.noreply.github.com>
2026-04-10 19:32:26 -0500

    scripts folder added

---
+++ Assets/scripts/player scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public GameObject Player;
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/player scripts/inputedActions.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class inputedActions : MonoBehaviour
+{
+    public static inputActions Instance {get; private set;}
+    public Plyerinput.playerActions input;
+
+    private void Awake()
+    {
+        Instance = this;
+        var inputs= new 
+    } 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
